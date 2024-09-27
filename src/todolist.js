d148af477b7a555fc55a573c6fc0dedf8b1ae9a9
eliascsoares <40612619+eliascsoares@users.noreply.github.com>
2024-09-27 11:46:33 +0100

    Initial commit: To Do List  Project

---
+++ src/todolist.js
@@ -0,0 +1,59 @@
+import React, { useState } from 'react';
+import './todolist.css';
+
+const TodoList = () => {
+  const [todos, setTodos] = useState([]);
+  const [input, setInput] = useState('');
+
+  const addTodo = () => {
+    if (input.trim() !== '') {
+      setTodos([...todos, { text: input, completed: false }]);
+      setInput('');
+    }
+  };
+
+  // ADD ENTER KEY TO ADD TASK 
+  const handleKeyPress = (e) => {
+    if (e.key === 'Enter') {
+      addTodo();
+    }
+  };
+
+  const deleteTodo = (index) => {
+    const newTodos = todos.filter((_, i) => i !== index);
+    setTodos(newTodos);
+  };
+
+  const toggleTodo = (index) => {
+    const newTodos = [...todos];
+    newTodos[index].completed = !newTodos[index].completed;
+    setTodos(newTodos);
+  };
+
+  return (
+    <div className="todo-list">
+      <div className="todo-input">
+        <input
+          type="text"
+          value={input}
+          onChange={(e) => setInput(e.target.value)}
+          onKeyDown = {handleKeyPress} //ENTER KEY CHECKER 
+          placeholder="Add a new task"
+        />
+        <button onClick={addTodo}>Add</button>
+      </div>
+      <ul>
+        {todos.map((todo, index) => (
+          <li key={index} className={todo.completed ? 'completed' : ''}>
+            <span onClick={() => toggleTodo(index)}>
+              {todo.text}
+            </span>
+            <button onClick={() => deleteTodo(index)}>Delete</button>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default TodoList;
